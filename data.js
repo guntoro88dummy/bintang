b880ef221aac90772af815819b3264fd210db3bc
guntoro88dummy <guntoro88@gmail.com>
2026-03-29 13:28:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,30 +1,27 @@
 const DATA = {
-
-shorts: [
-{ id: "aqz-KE-bpKQ", title: "Short 1" },
-{ id: "ysz5S6PUM-U", title: "Short 2" },
-{ id: "ScMzIvxBSi4", title: "Short 3" },
-{ id: "FTQbiNvZqaY", title: "Short 4" },
-{ id: "kXYiU_JCYtU", title: "Short 5" },
-{ id: "L_jWHffIx5E", title: "Short 6" }
+shorts:[
+{id:"ysz5S6PUM-U",title:"Short 1"},
+{id:"aqz-KE-bpKQ",title:"Short 2"},
+{id:"dQw4w9WgXcQ",title:"Short 3"},
+{id:"3JZ_D3ELwOQ",title:"Short 4"},
+{id:"L_jWHffIx5E",title:"Short 5"},
+{id:"V-_O7nl0Ii0",title:"Short 6"}
 ],
-
-videos: [
-{ id: "kJQP7kiw5Fk", title: "Video 1" },
-{ id: "9No-FiEInLA", title: "Video 2" },
-{ id: "CevxZvSJLk8", title: "Video 3" },
-{ id: "hTWKbfoikeg", title: "Video 4" },
-{ id: "3AtDnEC4zak", title: "Video 5" },
-{ id: "04854XqcfCY", title: "Video 6" }
+videos:[
+{id:"ysz5S6PUM-U",title:"Video 1"},
+{id:"aqz-KE-bpKQ",title:"Video 2"},
+{id:"dQw4w9WgXcQ",title:"Video 3"},
+{id:"3JZ_D3ELwOQ",title:"Video 4"},
+{id:"L_jWHffIx5E",title:"Video 5"},
+{id:"V-_O7nl0Ii0",title:"Video 6"},
+{id:"Zi_XLOBDo_Y",title:"Video 7"}
 ],
-
-live: [
-{ id: "21X5lGlDOfg", title: "Live 1" },
-{ id: "DWcJFNfaw9c", title: "Live 2" },
-{ id: "e-ORhEE9VVg", title: "Live 3" },
-{ id: "uelHwf8o7_U", title: "Live 4" },
-{ id: "YqeW9_5kURI", title: "Live 5" },
-{ id: "5qap5aO4i9A", title: "Live 6" }
+live:[
+{id:"5qap5aO4i9A",title:"Live 1"},
+{id:"DWcJFNfaw9c",title:"Live 2"},
+{id:"21X5lGlDOfg",title:"Live 3"},
+{id:"hHW1oY26kxQ",title:"Live 4"},
+{id:"jfKfPfyJRdk",title:"Live 5"},
+{id:"e3L1PIY1pN8",title:"Live 6"}
 ]
-
 };
