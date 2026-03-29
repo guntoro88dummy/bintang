74fb24f82f13f74e5d6f6d3c47de968255358339
guntoro88dummy <guntoro88@gmail.com>
2026-03-29 12:18:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,52 +2,52 @@ const hero = document.getElementById("hero-video");
 const shorts = document.getElementById("shorts");
 const videos = document.getElementById("videos");
 const live = document.getElementById("live");
+const gridMain = document.getElementById("grid-main");
 
 // HERO RANDOM
 const allVideos = [...DATA.videos, ...DATA.live];
 const random = allVideos[Math.floor(Math.random()*allVideos.length)];
 
 hero.innerHTML = `
-<iframe src="https://www.youtube.com/embed/${random.id}" allowfullscreen></iframe>
+<iframe src="https://www.youtube.com/embed/${random.id}?autoplay=1&mute=1" allowfullscreen></iframe>
 `;
 
-// RENDER FUNCTION
-function render(list, container, isScroll=false){
+// RENDER
+function render(list, container){
 
 list.forEach(v => {
 
 const thumb = `https://img.youtube.com/vi/${v.id}/mqdefault.jpg`;
 
-container.innerHTML += `
-<div class="card"
-onmouseover="playPreview(this,'${v.id}')"
-onmouseout="stopPreview(this,'${thumb}')">
+const el = document.createElement("div");
+el.className = "card";
 
+el.innerHTML = `
 <img src="${thumb}">
 <p>${v.title}</p>
-
-</div>
 `;
+
+el.addEventListener("mouseover", () => {
+el.innerHTML = `
+<iframe src="https://www.youtube.com/embed/${v.id}?autoplay=1&mute=1" allowfullscreen></iframe>
+`;
+});
+
+el.addEventListener("mouseout", () => {
+el.innerHTML = `
+<img src="${thumb}">
+<p>${v.title}</p>
+`;
+});
+
+container.appendChild(el);
 
 });
 
 }
 
-// PREVIEW HOVER (PLAY VIDEO)
-function playPreview(el, id){
-el.innerHTML = `
-<iframe src="https://www.youtube.com/embed/${id}?autoplay=1&mute=1"
-allowfullscreen></iframe>
-`;
-}
-
-function stopPreview(el, thumb){
-el.innerHTML = `
-<img src="${thumb}">
-`;
-}
-
 // LOAD
-render(DATA.shorts, shorts, true);
+render(DATA.grid, gridMain);
+render(DATA.shorts, shorts);
 render(DATA.videos, videos);
 render(DATA.live, live);
