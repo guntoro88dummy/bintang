d01ea134999c2083e03d3f0866281ee05354fcbd
guntoro88dummy <guntoro88@gmail.com>
2026-03-29 15:03:57 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,33 @@
 const DATA = {
-shorts:[
-{id:"ysz5S6PUM-U",title:"Short 1"},
-{id:"aqz-KE-bpKQ",title:"Short 2"},
-{id:"dQw4w9WgXcQ",title:"Short 3"},
-{id:"3JZ_D3ELwOQ",title:"Short 4"},
-{id:"L_jWHffIx5E",title:"Short 5"},
-{id:"V-_O7nl0Ii0",title:"Short 6"}
-],
-videos:[
-{id:"ysz5S6PUM-U",title:"Video 1"},
-{id:"aqz-KE-bpKQ",title:"Video 2"},
-{id:"dQw4w9WgXcQ",title:"Video 3"},
-{id:"3JZ_D3ELwOQ",title:"Video 4"},
-{id:"L_jWHffIx5E",title:"Video 5"},
-{id:"V-_O7nl0Ii0",title:"Video 6"},
-{id:"Zi_XLOBDo_Y",title:"Video 7"}
-],
-live:[
-{id:"5qap5aO4i9A",title:"Live 1"},
-{id:"DWcJFNfaw9c",title:"Live 2"},
-{id:"21X5lGlDOfg",title:"Live 3"},
-{id:"hHW1oY26kxQ",title:"Live 4"},
-{id:"jfKfPfyJRdk",title:"Live 5"},
-{id:"e3L1PIY1pN8",title:"Live 6"}
-]
+  videos: [
+    "b0Q3yEwEnfY",
+    "eY2KD0u6N14",
+    "8bS_VyqiV4",
+    "iF4il4x2P3k",
+    "FaX7QasxsqQ",
+    "wlG1Q8wrhBQ",
+    "IkZEZaUPA4M",
+    "XT6BOP09tIk",
+    "_LRMLRjExg4"
+  ],
+  shorts: [
+    "9mS063gtcs0",
+    "zMWRHswI9fI",
+    "uuPMHF7ADfg",
+    "r_hyYB2FQUY",
+    "W2bVatf10vU",
+    "am1Zg2TH_wE",
+    "W34n4yM4pak",
+    "mQEJayHSq2I",
+    "zuCGRkifN38"
+  ],
+  live: [
+    "xgwqcDSmStg",
+    "j02unXQy5tw",
+    "15EDEOtnSHY",
+    "MTOQhiJI6mU",
+    "eFg6X-0pBBA",
+    "ZKsMLgXJoFg",
+    "KT_OaumRJbQ"
+  ]
 };
