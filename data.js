ab020bee22dfa0872a0e4568f0f98c7c6f4d3207
guntoro88dummy <guntoro88@gmail.com>
2026-03-29 12:16:13 +0700

    data.js

---
+++ data.js
@@ -0,0 +1,39 @@
+const DATA = {
+
+shorts: [
+{ id: "w8rYQ40C9xo", title: "Short 1" },
+{ id: "5qap5aO4i9A", title: "Short 2" },
+{ id: "aqz-KE-bpKQ", title: "Short 3" },
+{ id: "ysz5S6PUM-U", title: "Short 4" },
+{ id: "ScMzIvxBSi4", title: "Short 5" },
+{ id: "FTQbiNvZqaY", title: "Short 6" }
+],
+
+videos: [
+{ id: "kJQP7kiw5Fk", title: "Video Terbaru 1" },
+{ id: "9No-FiEInLA", title: "Video Terbaru 2" },
+{ id: "CevxZvSJLk8", title: "Video Terbaru 3" },
+{ id: "hTWKbfoikeg", title: "Video Terbaru 4" },
+{ id: "3AtDnEC4zak", title: "Video Terbaru 5" },
+{ id: "04854XqcfCY", title: "Video Terbaru 6" }
+],
+
+live: [
+{ id: "21X5lGlDOfg", title: "Live Stream 1" },
+{ id: "5qap5aO4i9A", title: "Live Stream 2" },
+{ id: "DWcJFNfaw9c", title: "Live Stream 3" },
+{ id: "e-ORhEE9VVg", title: "Live Stream 4" },
+{ id: "uelHwf8o7_U", title: "Live Stream 5" },
+{ id: "YqeW9_5kURI", title: "Live Stream 6" }
+],
+
+grid: [
+{ id: "kJQP7kiw5Fk", title: "Highlight 1" },
+{ id: "9No-FiEInLA", title: "Highlight 2" },
+{ id: "CevxZvSJLk8", title: "Highlight 3" },
+{ id: "hTWKbfoikeg", title: "Highlight 4" },
+{ id: "3AtDnEC4zak", title: "Highlight 5" },
+{ id: "04854XqcfCY", title: "Highlight 6" }
+]
+
+};
