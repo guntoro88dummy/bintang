e8eae352829670c177d1679ae9b429f4a6b86b52
guntoro88dummy <guntoro88@gmail.com>
2026-03-31 21:50:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -94,3 +94,47 @@ renderGrid(shorts, "shorts", true);
 renderGrid(videos, "videos");
 renderGrid(live, "live");
 renderTrending(trending);
+
+
+// =======================
+// JADWAL WAYANG
+// =======================
+
+const today = new Date();
+
+const tanggal = today.toLocaleDateString('id-ID',{
+day:'numeric',
+month:'long',
+year:'numeric'
+});
+
+document.getElementById("tanggalHariIni").innerHTML =
+"[ "+tanggal+" ]";
+
+fetch("https://api.rss2json.com/v1/api.json?rss_url=https://www.kluban.net/feeds/posts/default")
+
+.then(res=>res.json())
+
+.then(data=>{
+
+let html="";
+
+data.items.slice(0,3).forEach(item=>{
+
+if(item.title.toLowerCase().includes("wayang")){
+
+html+=`
+
+<div class="jadwal-item">
+${item.title}
+</div>
+
+`;
+
+}
+
+});
+
+document.getElementById("jadwalWayang").innerHTML = html;
+
+});
