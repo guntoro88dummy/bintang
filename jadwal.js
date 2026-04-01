6ea93a118e15bf21921d46eb8b1856c8e4d629e8
guntoro88dummy <guntoro88@gmail.com>
2026-04-01 14:01:43 +0700

    Add files via upload

---
+++ jadwal.js
@@ -0,0 +1,56 @@
+const JADWAL_WAYANG = [
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Bangun Sukarso",
+    "bintang": "",
+    "lokasi": "Congot, Kaliori, Kalibagor, Banyumas"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Eko Suwaryo",
+    "bintang": "",
+    "lokasi": "Karangbenda, Adipala, Cilacap"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Raden Djagad Arroyan",
+    "bintang": "Sinden Taji & Memet",
+    "lokasi": "Palon, Jepon, Blora"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Kasim Purwo Wasito",
+    "bintang": "Ki Solar Dkk",
+    "lokasi": "Candirejo, Pomah, Tulung, Klaten"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Bagong Darmono",
+    "bintang": "",
+    "lokasi": "Ds Guneman, Dk Randusari, Kec Prambanan, Klaten"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Hariyo Adi Priambodo (Mekar Wijaya)",
+    "bintang": "Ratu Mewah & Azzam N Muzijat",
+    "lokasi": "Dusun. Tanjungan, Ds. Tanjungan, Kec. Kemlagi, Kab. Mojokerto"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki RM Akbar Syahalam S.Sn",
+    "bintang": "Cak Komet Cs & Lusi Brahman",
+    "lokasi": "Ds. Ketawang, Kec. Gondang, Kab. Nganjuk"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Senoaji (Kridho Budoyo)",
+    "bintang": "",
+    "lokasi": "Kalitangi Wates Umpak, Kec. Trowulan, Mojokerto"
+  },
+  {
+    "tanggal": "1 April 2026",
+    "dalang": "Ki Puguh Prasetyo",
+    "bintang": "",
+    "lokasi": " Ngabetan, Cerme, Gresik - Ki Puguh Prasetyo"
+  }
+]
